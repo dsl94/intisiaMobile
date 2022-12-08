74ba6c22972fcc1d7551adff424a35eb33ff17f3
nemanja <nemanja.petrovic@united.cloud>
2022-12-08 19:20:22 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,17 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
+import {AuthInterceptor} from "./helpers/auth.interceptor";
+import {environment} from "../environments/environment";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  imports: [BrowserModule, IonicModule.forRoot(),HttpClientModule, AppRoutingModule],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
+    { provide: 'BASE_API_URL', useValue: environment.apiUrl },
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ src/app/pages/login/login.page.ts
@@ -0,0 +1,70 @@
+import { Component, OnInit } from '@angular/core';
+import {AuthService} from "../../services/auth.service";
+import {TokenService} from "../../services/token.service";
+import {Router} from "@angular/router";
+import {AlertController} from "@ionic/angular";
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+
+  form: any = {
+    username: null,
+    password: null
+  };
+  isLoggedIn = false;
+  isLoginFailed = false;
+  errorMessage = '';
+  roles: string[] = [];
+
+  constructor(private authService: AuthService, private tokenService: TokenService, private router: Router,
+              private alertController: AlertController) { }
+
+  ngOnInit(): void {
+    if (this.tokenService.getToken()) {
+      this.isLoggedIn = true;
+      this.roles = this.tokenService.getUser().roles;
+    }
+  }
+
+  onSubmit(): void {
+    const { username, password } = this.form;
+
+    this.authService.login(username, password).subscribe(
+        (data: { token: any; }) => {
+        this.tokenService.saveToken(data.token);
+        this.tokenService.saveUser(data);
+
+        this.isLoginFailed = false;
+        this.isLoggedIn = true;
+        this.roles = this.tokenService.getUser().roles;
+        if(this.roles.includes('ROLE_SYSTEM_ADMIN')) {
+          this.router.navigate(['/admin/users']);
+        } else if (this.roles.includes('ROLE_ADMIN')) {
+          this.router.navigate(['/app/tabs/tab1']);
+        } else {
+          this.router.navigate(['/user/profile']);
+        }
+      },
+        (err: any) => {
+          console.log(err);
+        this.isLoginFailed = true;
+        this.presentAlert().then();
+      }
+    );
+  }
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Bad credentials',
+      message: 'Please check your credentials',
+      buttons: ['OK'],
+    });
+
+    await alert.present();
+  }
+
+}
