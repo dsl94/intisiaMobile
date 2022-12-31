2d296243d2343f1f188745303d4cdb2ed1ee1568
nemanja <nemanja.petrovic@united.cloud>
2022-12-31 13:59:18 +0100

    Added map

--- src/app/pages/flight-details/flight-details.page.ts
+++ src/app/pages/flight-details/flight-details.page.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
 import {Flight} from "../../models/flight.model";
 import {FlightService} from "../../services/flight.service";
 import {ActivatedRoute} from "@angular/router";
 import {LoadingController} from "@ionic/angular";
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-flight-details',
@@ -13,6 +14,80 @@ export class FlightDetailsPage implements OnInit {
   id!: number;
   flight!: Flight;
   loaded = false;
+  private map: any;
+
+  private initMap(): void {
+    const departureIcon = L.divIcon({
+      html:
+        '<ion-text color="success"><i class="fas fa-plane-departure fa-3x"></i></ion-text>',
+      iconSize: [20, 20],
+      className: 'myDivIcon',
+    });
+    const arrivallIcon = L.divIcon({
+      html:
+        '<ion-text color="danger"><i class="fas fa-plane-arrival fa-3x"></i></ion-text>',
+      iconSize: [20, 20],
+      className: 'myDivIcon',
+    });
+    const deplon = this.flight.deplon;
+    const deplat = this.flight.deplat;
+    const arlon = this.flight.arlon;
+    const arlat = this.flight.arlat;
+    this.map = L.map('map', {
+      center: [deplat, deplon],
+      zoom: 3,
+    }).fitBounds(
+      [
+        [deplat, deplon],
+        [arlat, arlon],
+      ],
+      { padding: [50, 50] }
+    );
+
+
+    const tiles = L.tileLayer(
+      'https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png',
+      {
+        maxZoom: 18,
+        minZoom: 3,
+        attribution:
+          '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors',
+      }
+    );
+
+    tiles.addTo(this.map);
+
+    const depMarker = L.marker([deplat, deplon], {
+      title: this.flight.departure,
+      icon: departureIcon,
+    });
+
+    depMarker.addTo(this.map);
+    depMarker
+      .bindPopup(this.flight.departure, {
+        autoClose: false,
+        closeOnClick: false,
+      })
+      .openPopup();
+
+    const arrMarker = L.marker([arlat, arlon], {
+      title: this.flight.arrival,
+      icon: arrivallIcon,
+    });
+
+    arrMarker.addTo(this.map);
+    arrMarker
+      .bindPopup(this.flight.arrival, { autoClose: false, closeOnClick: false })
+      .openPopup();
+
+    var polylinePoints = [
+      [deplat, deplon],
+      [arlat, arlon],
+    ];
+
+    // @ts-ignore
+    var polyline = L.polyline(polylinePoints).addTo(this.map);
+  }
 
   constructor(  private flightService: FlightService,
                 private activatedRoute: ActivatedRoute,
@@ -33,6 +108,7 @@ export class FlightDetailsPage implements OnInit {
       this.flight = data;
       this.loaded = true;
       loading.dismiss();
+      this.initMap();
     });
   }
 }
